e5268578075959949b67adbc8f32433ca0e7b80d
Chao Du <duchao@rd.netease.com>
2017-08-12 14:02:35 +0800

    FIX:修改打包配置

--- app/src/main/java/com/youdao/androidtest/MainActivity.java
+++ app/src/main/java/com/youdao/androidtest/MainActivity.java
@@ -2,12 +2,25 @@ package com.youdao.androidtest;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
+
+    private MetaConfig metaConfig;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        metaConfig.readFromManifest(this);
+
+        TextView textView = (TextView) findViewById(R.id.unmneg_channle_value);
+        textView.setText(metaConfig.mUmengChannel);
+        textView = (TextView) findViewById(R.id.vender_value);
+        textView.setText(metaConfig.mVendor);
+        textView = (TextView) findViewById(R.id.bundle_value);
+        textView.setText(String.valueOf(metaConfig.mCheckBundle));
+        textView = (TextView) findViewById(R.id.debug_value);
+        textView.setText(String.valueOf(metaConfig.mIsDebug));
     }
 }
---
+++ app/src/main/java/com/youdao/androidtest/MetaConfig.java
@@ -0,0 +1,35 @@
+package com.youdao.androidtest;
+
+import android.content.Context;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
+
+/**
+ * Created by duchao on 2017/8/12.
+ */
+
+public class MetaConfig {
+    public boolean mIsDebug = false;
+    public String mVendor = "undefined";
+    public boolean mCheckBundle = false;
+    public boolean mIncludeVendorLogo = false;
+    public String mUmengChannel = "undefined";
+
+    public static MetaConfig readFromManifest(Context context) {
+        MetaConfig config = new MetaConfig();
+        ApplicationInfo appInfo = null;
+        try {
+            appInfo = context.getPackageManager().getApplicationInfo(
+                    context.getPackageName(), PackageManager.GET_META_DATA);
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+        config.mIsDebug = appInfo.metaData.getBoolean("debug", false);
+        config.mCheckBundle = appInfo.metaData.getBoolean("check_bundle", false);
+        config.mIncludeVendorLogo = appInfo.metaData.getBoolean("include_vendor_logo");
+        config.mVendor = appInfo.metaData.getString("vender");
+        config.mUmengChannel = appInfo.metaData.getString("UMENG_CHANNEL");
+        return config;
+    }
+
+}
